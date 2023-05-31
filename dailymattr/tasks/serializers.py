65d6dbe5a64d220fbdc7b2427dce93da804027e2
Rostislav Podgorniy <podgorniy.inc@gmail.com>
2023-05-31 15:03:35 +0300

    Added manual serialization of objects of the Tasks model

--- dailymattr/tasks/serializers.py
+++ dailymattr/tasks/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from datetime import date
 from .models import Tasks
 
 
-class TasksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tasks
-        fields = ('__all__')
+class TasksSerializer(serializers.Serializer):
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    due_date = serializers.DateField(default=date.today)
+    description = serializers.CharField(max_length=140)
+    task_comment = serializers.CharField(max_length=140)
 
+    task_numb = serializers.CharField(max_length=32)
+    priority = serializers.CharField(max_length=12)
+    link = serializers.URLField()
+    card_id = serializers.IntegerField(read_only=True)
+    status_id = serializers.IntegerField(default=1)
--- dailymattr/tasks/views.py
+++ dailymattr/tasks/views.py
@@ -3,15 +3,20 @@ from datetime import date
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Tasks, Cards
-# from .serializers import TasksSerializer
+from .serializers import TasksSerializer
 
 
 class TasksAPIView(APIView):
     def get(self, request):
-        return Response(Tasks.objects.all().values())
+        t = Tasks.objects.all()
+        return Response(TasksSerializer(t, many=True).data)
 
 
     def post(self, request):
+        serializer = TasksSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+
         def get_card():
             try:
                 post_date = request.data['due_date']
@@ -23,6 +28,7 @@ class TasksAPIView(APIView):
 
 
         post_new = Tasks.objects.create(
+                due_date = request.data['due_date'],
                 task_numb = request.data['task_numb'],
                 priority = request.data['priority'],
                 link = request.data['link'],
